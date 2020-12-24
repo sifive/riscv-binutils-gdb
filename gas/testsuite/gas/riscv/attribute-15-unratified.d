b70edfe3364f8fbe4e2cdcf3a0c96a1e8632a3b8
Nelson Chu <nelson.chu@sifive.com>
2020-12-24 11:14:18 +0800

    Delete branch sifive-rvv-1.0.x-zfh-obsolete for archive.

--- gas/testsuite/gas/riscv/attribute-15-unratified.d
+++
@@ -1,6 +0,0 @@
-#as: -march-attr -march=rv32ifv_zfh_zvamo_zvediv_zvlsseg_zvqmac
-#readelf: -A
-#source: empty.s
-Attribute Section: riscv
-File Attributes
-  Tag_RISCV_arch: ".*_v1p0_zfh0p1_zvamo1p0_zvediv1p0_zvlsseg1p0_zvqmac1p0"
