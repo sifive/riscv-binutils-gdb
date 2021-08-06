e9916bcaf0bdfe2c0001d00d807c45ece3465299
Nelson Chu <nelson.chu@sifive.com>
2021-08-06 19:56:06 +0800

    SiFive: Reorder the prefixed extensions which are out of order.
    bfd/
    	* elfxx-riscv.c (riscv_parse_prefixed_ext): Help to reorder
    	the prefixed extensions if users set the wrong order.
    	(riscv_parse_subset): Updated.
    gas/
    	* testsuite/gas/riscv/march-fail-order-x-z.d: Removed.
    	* testsuite/gas/riscv/march-fail-order-x-z.l: Likewise.
    	* testsuite/gas/riscv/march-fail-order-x.d: Likewise.
    	* testsuite/gas/riscv/march-fail-order-x.l: Likewise.
    	* testsuite/gas/riscv/march-fail-order-z.d: Likewise.
    	* testsuite/gas/riscv/march-fail-order-z.l: Likewise.
    	* testsuite/gas/riscv/attribute-prefixed-reorder.d: New testcase.
    	Reorder the prefixed extensions which are out of order.

--- bfd/elfxx-riscv.c
+++ bfd/elfxx-riscv.c
@@ -1794,7 +1794,6 @@ riscv_parse_prefixed_ext (riscv_parse_subset_t *rps,
 {
   int major_version;
   int minor_version;
-  const char *last_name;
   enum riscv_prefix_ext_class class;
 
   while (*p)
@@ -1867,28 +1866,6 @@ riscv_parse_prefixed_ext (riscv_parse_subset_t *rps,
 	  rps->error_handler
 	    (_("%s: unknown prefixed ISA extension `%s'"),
 	     arch, subset);
-	  free (subset);
-	  return NULL;
-	}
-
-      /* Check that the extension isn't duplicate.  */
-      last_name = rps->subset_list->tail->name;
-      if (!strcasecmp (last_name, subset))
-	{
-	  rps->error_handler
-	    (_("%s: duplicate prefixed ISA extension `%s'"),
-	     arch, subset);
-	  free (subset);
-	  return NULL;
-	}
-
-      /* Check that the extension is in expected order.  */
-      if (riscv_compare_subsets (last_name, subset) > 0)
-	{
-	  rps->error_handler
-	    (_("%s: prefixed ISA extension `%s' is not in expected "
-	       "order.  It must come before `%s'"),
-	     arch, subset, last_name);
 	  free (subset);
 	  return NULL;
 	}
@@ -2084,7 +2061,8 @@ riscv_parse_subset (riscv_parse_subset_t *rps,
   if (p == NULL)
     return false;
 
-  /* Parse the different classes of extensions in the specified order.  */
+  /* Parse the prefixed extensions, we will help to reorder them to
+     the correct order.  */
   while (*p != '\0')
     {
       p = riscv_parse_prefixed_ext (rps, arch, p);
---
+++ gas/testsuite/gas/riscv/attribute-prefixed-reorder.d
@@ -0,0 +1,7 @@
+#as: -misa-spec=20191213 -march-attr -march=rv64gcv_zbc_zfh_zba_zifencei_xbargle2p0_zbb_zicsr_xargle2p0
+#readelf: -A
+#source: empty.s
+
+Attribute Section: riscv
+File Attributes
+  Tag_RISCV_arch: "rv64i2p1_m2p0_a2p1_f2p2_d2p2_c2p0_v1p0_zicsr2p0_zifencei2p0_zfh0p1_zba1p0_zbb1p0_zbc1p0_zve32f1p0_zve32x1p0_zve64d1p0_zve64f1p0_zve64x1p0_zvl128b1p0_zvl32b1p0_zvl64b1p0_xargle2p0_xbargle2p0"
