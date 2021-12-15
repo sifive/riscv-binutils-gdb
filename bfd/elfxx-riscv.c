6b03be2640a95d9ef873228d3f23e1da2082f833
Nelson Chu <nelson.chu@sifive.com>
2021-12-15 17:23:11 +0800

    SiFive: Separated part of the zfh to zfhmin, and zfh also imply zfhmin.
    Here is the ISA spec,
    https://github.com/riscv/riscv-isa-manual/blob/draft-20211213-ea38395/src/zfh.tex#L392
    
    The spec said that zfhmin includes,
    
    * FLH, FSH, FMV.X.H, FMV.H.X, FCVT.S.H, and FCVT.H.S
    
    If D is enabled, then zfhmin includes,
    
    * FCVT.D.H and FCVT.H.D
    
    If Q is enabled, then zfhmin also includes,
    
    * FCVT.Q.H and FCVT.H.Q

--- bfd/elfxx-riscv.c
+++ bfd/elfxx-riscv.c
@@ -1152,6 +1152,7 @@ static struct riscv_implicit_subset riscv_implicit_subsets[] =
   {"f", "zicsr",	check_implicit_always},
   {"zfh", "f",		check_implicit_always},
   {"zfh", "zicsr",	check_implicit_always},
+  {"zfh", "zfhmin",	check_implicit_always},
   {"zqinx", "zdinx",	check_implicit_always},
   {"zdinx", "zfinx",	check_implicit_always},
   {"zk", "zkn",		check_implicit_always},
@@ -1237,6 +1238,7 @@ static struct riscv_supported_ext riscv_supported_std_z_ext[] =
   {"zifencei",		ISA_SPEC_CLASS_20190608,	2, 0,  0 },
   {"zihintpause",	ISA_SPEC_CLASS_DRAFT,		1, 0,  0 },
   {"zfh",		ISA_SPEC_CLASS_DRAFT,		0, 1,  0 },
+  {"zfhmin",		ISA_SPEC_CLASS_DRAFT,		0, 1,  0 },
   {"zfinx",		ISA_SPEC_CLASS_DRAFT,		1, 0,  0 },
   {"zdinx",		ISA_SPEC_CLASS_DRAFT,		1, 0,  0 },
   {"zqinx",		ISA_SPEC_CLASS_DRAFT,		1, 0,  0 },
@@ -2410,12 +2412,14 @@ riscv_multi_subset_supports (riscv_parse_subset_t *rps,
 	      || riscv_subset_supports (rps, "zqinx"));
     case INSN_CLASS_ZFH:
       return riscv_subset_supports (rps, "zfh");
-    case INSN_CLASS_D_AND_ZFH:
+    case INSN_CLASS_ZFHMIN:
+      return riscv_subset_supports (rps, "zfhmin");
+    case INSN_CLASS_D_AND_ZFHMIN:
       return (riscv_subset_supports (rps, "d")
-	      && riscv_subset_supports (rps, "zfh") );
-    case INSN_CLASS_Q_AND_ZFH:
+	      && riscv_subset_supports (rps, "zfhmin") );
+    case INSN_CLASS_Q_AND_ZFHMIN:
       return (riscv_subset_supports (rps, "q")
-	      && riscv_subset_supports (rps, "zfh"));
+	      && riscv_subset_supports (rps, "zfhmin"));
     case INSN_CLASS_ZBA:
       return riscv_subset_supports (rps, "zba");
     case INSN_CLASS_ZBB:
