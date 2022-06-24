1b57e363fbf9e0ac3a83659ec0a1aad0d4cca059
Nelson Chu <nelson.chu@sifive.com>
2022-06-24 10:19:11 +0800

    SiFive: Updated the version of sscofpmf to 1.0.
    [spec] https://github.com/riscv/riscv-count-overflow/releases/tag/v1.0.0
    [jira] https://sifive.atlassian.net/browse/SCT-756
           https://sifive.atlassian.net/browse/SCT-1461

--- gas/testsuite/gas/riscv/attribute-prefixed-reorder.d
+++ gas/testsuite/gas/riscv/attribute-prefixed-reorder.d
@@ -1,7 +1,7 @@
-#as: -misa-spec=20191213 -march-attr -march=rv64gcv_zbc_zfh_zba_zifencei_xbargle2p0_zbb_zicsr_xargle2p0
+#as: -misa-spec=20191213 -march-attr -march=rv64gcv_zbc_zfh_zba_zifencei_xbargle2p0_zbb_zicsr_xargle2p0_sscofpmf
 #readelf: -A
 #source: empty.s
 
 Attribute Section: riscv
 File Attributes
-  Tag_RISCV_arch: "rv64i2p1_m2p0_a2p1_f2p2_d2p2_c2p0_v1p0_zicsr2p0_zifencei2p0_zfh0p1_zfhmin0p1_zba1p0_zbb1p0_zbc1p0_zve32f1p0_zve32x1p0_zve64d1p0_zve64f1p0_zve64x1p0_zvl128b1p0_zvl32b1p0_zvl64b1p0_xargle2p0_xbargle2p0"
+  Tag_RISCV_arch: "rv64i2p1_m2p0_a2p1_f2p2_d2p2_c2p0_v1p0_zicsr2p0_zifencei2p0_zfh1p0_zfhmin1p0_zba1p0_zbb1p0_zbc1p0_zve32f1p0_zve32x1p0_zve64d1p0_zve64f1p0_zve64x1p0_zvl128b1p0_zvl32b1p0_zvl64b1p0_sscofpmf1p0_xargle2p0_xbargle2p0"
