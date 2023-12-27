63e2b847e929a38a31715ca12d4b0b4fb1c70160
Hau Hsu <hau.hsu@sifive.com>
2023-12-27 17:26:46 +0800

    Update cfilp plt test case

--- ld/testsuite/ld-riscv-elf/zicfilp-plt.d
+++ ld/testsuite/ld-riscv-elf/zicfilp-plt.d
@@ -1,5 +1,6 @@
 #name: Check --force-zicfilp emits landing pad PLT (shared)
 #source: zicfilp-plt.s
+#as: -march=rv64gc_zicfilp
 #ld: -shared -z force-zicfilp
 #objdump: -dr -j .plt
 
@@ -8,7 +9,7 @@
 Disassembly of section \.plt:
 
 [0-9a-f]+ <\.plt>:
-.*:[ 	]+00000097[ 	]+auipc[ 	]+ra,0x0
+.*:[ 	]+00000017[ 	]+lpad[ 	]+0x0
 .*:[ 	]+00001e97[ 	]+auipc[ 	]+t4,0x1
 .*:[ 	]+41c30333[ 	]+sub[ 	]+t1,t1,t3
 .*:[ 	]+1e4ebe03[ 	]+ld[ 	]+t3,484\(t4\) # 1488 <\.got>
@@ -22,7 +23,7 @@ Disassembly of section \.plt:
 .*:[ 	]+00000013[ 	]+nop
 
 [0-9a-f]+ <foo@plt>:
-.*:[ 	]+00001097[ 	]+auipc[ 	]+ra,0x1
+.*:[ 	]+00001017[ 	]+lpad[ 	]0x1
 .*:[ 	]+00001e17[ 	]+auipc[ 	]+t3,0x1
 .*:[ 	]+1c4e3e03[ 	]+ld[ 	]+t3,452\(t3\) # 1498 <foo>
 .*:[ 	]+000013b7[ 	]+lui[ 	]+t2,0x1
@@ -32,7 +33,7 @@ Disassembly of section \.plt:
 .*:[ 	]+00000013[ 	]+nop
 
 [0-9a-f]+ <bar@plt>:
-.*:[ 	]+00001097[ 	]+auipc[ 	]+ra,0x1
+.*:[ 	]+00001017[ 	]+lpad[ 	]+0x1
 .*:[ 	]+00001e17[ 	]+auipc[ 	]+t3,0x1
 .*:[ 	]+1ace3e03[ 	]+ld[ 	]+t3,428\(t3\) # 14a0 <bar>
 .*:[ 	]+000013b7[ 	]+lui[ 	]+t2,0x1
