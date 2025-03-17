8dc480a2baa750f51644981c641fde93e28fcee9
Jerry Zhang Jian <jerry.zhangjian@sifive.com>
2025-03-17 15:00:17 +0800

    SiFive: add Ssstrict 1.0 (#100)
    Signed-off-by: Jerry Zhang Jian <jerry.zhangjian@sifive.com>

--- bfd/elfxx-riscv.c
+++ bfd/elfxx-riscv.c
@@ -1843,6 +1843,7 @@ static struct riscv_supported_ext riscv_supported_std_s_ext[] =
   {"sdext",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
   {"sdtrig",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
   {"ssqosid",           ISA_SPEC_CLASS_DRAFT,           1, 0, 0 },
+  {"ssstrict",           ISA_SPEC_CLASS_DRAFT,           1, 0, 0 },
   {"svukte",		ISA_SPEC_CLASS_DRAFT,		0, 4, 0 },
   {NULL, 0, 0, 0, 0}
 };
