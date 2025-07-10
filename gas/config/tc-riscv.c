b3a140f32599f7a178bdfce8cc5366398d5db029
Kito Cheng <kito.cheng@sifive.com>
2025-07-10 16:39:46 +0800

    SiFive: Fix LPAD for eh_frame
    There is a white list for relocation may reduce the size which might
    lead the eh_frame info wrong, so seems like we should add LPAD
    relocation to that list as well.
    
    Although I don't really understand why...but Jim Willson did that years
    ago, so I believe he is right!
    
    See https://github.com/bminor/binutils-gdb/commit/f77bb6c56b4091ed863d8cd03333a79a8d554ce1

--- gas/config/tc-riscv.c
+++ gas/config/tc-riscv.c
@@ -2098,7 +2098,8 @@ append_insn (struct riscv_cl_insn *ip, expressionS *address_expr,
   if (reloc_type == BFD_RELOC_RISCV_HI20
       || reloc_type == BFD_RELOC_RISCV_PCREL_HI20
       || reloc_type == BFD_RELOC_RISCV_TPREL_HI20
-      || reloc_type == BFD_RELOC_RISCV_TPREL_ADD)
+      || reloc_type == BFD_RELOC_RISCV_TPREL_ADD
+      || reloc_type == BFD_RELOC_RISCV_LPAD)
     {
       frag_wane (frag_now);
       frag_new (0);
